47e252a6815688a33bdeb739bba3ab5db7c84de4
David Balatero <dbalatero@gmail.com>
2010-02-03 11:36:24 -0800

    Added set/get for last tweet

--- botbotbot.rb
+++ botbotbot.rb
@@ -4,9 +4,25 @@ require 'rubygems'
 require 'twitter'
 
 class BotBotBot
+  LAST_TWEET_FILE = 'last_tweet.txt'
+
   def self.search_results(last_tweet = nil)
     search = Twitter::Search.new.from('fixie_bot')
     search = search.since(last_tweet) if last_tweet
     search.fetch.results
+  end
+
+  def self.get_last_tweet
+    if File.exist?(LAST_TWEET_FILE)
+      File.read(LAST_TWEET_FILE).strip
+    else
+      nil
+    end
+  end
+
+  def self.set_last_tweet(last_tweet)
+    File.open(LAST_TWEET_FILE, "wb") do |fp|
+      fp.write(last_tweet)
+    end
   end
 end

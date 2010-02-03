1e8171e155eeed569c9cc2b9c6f37ad535436d0e
David Balatero <dbalatero@gmail.com>
2010-02-03 11:26:19 -0800

    Added search results method

--- botbotbot.rb
+++ botbotbot.rb
@@ -4,5 +4,11 @@ require 'rubygems'
 require 'twitter'
 
 module BotBotBot
+  def search_results(last_tweet = nil)
+    search = Twitter::Search.new.from('fixiebot')
+    search = search.since(last_tweet) if last_tweet
+    search.fetch.results
+  end
+
 
 end
